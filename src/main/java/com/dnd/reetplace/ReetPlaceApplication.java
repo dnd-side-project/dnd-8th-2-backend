96feef491910aad9460a9e28bd5a59313ee43895
Wo-ogie <siwall0105@gmail.com>
2023-02-14 17:48:06 +0900

    chore: 프로젝트 생성 및 기본 설정

---
+++ src/main/java/com/dnd/reetplace/ReetPlaceApplication.java
@@ -0,0 +1,13 @@
+package com.dnd.reetplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReetPlaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReetPlaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dnd/reetplace/ReetPlaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnd.reetplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReetPlaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
