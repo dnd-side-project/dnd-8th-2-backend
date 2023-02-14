6bf2344e2ad2b33cebe403ebfc3bb509c5115858
Wo-ogie <siwall0105@gmail.com>
2023-02-14 18:32:53 +0900

    [FEAT #3] 회원 Entity 구현
    회원은 soft delete 적용 대상이므로 `@SQLDelete()`를 적용하여 실제로 삭제되지 않도록 적용하였음.

---
+++ src/main/java/com/dnd/reetplace/app/domain/Member.java
@@ -0,0 +1,45 @@
+package com.dnd.reetplace.app.domain;
+
+import com.dnd.reetplace.app.domain.common.BaseTimeEntity;
+import com.dnd.reetplace.app.type.LoginType;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@SQLDelete(sql = "UPDATE member SET deleted_at = CURRENT_TIMESTAMP WHERE member_id = ?")
+@Entity
+public class Member extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String uid;
+
+    @Enumerated(EnumType.STRING)
+    private LoginType loginType;
+
+    private String email;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    private LocalDateTime deletedAt;
+
+    @Builder
+    private Member(String uid, LoginType loginType, String email, String nickname) {
+        this.uid = uid;
+        this.loginType = loginType;
+        this.email = email;
+        this.nickname = nickname;
+    }
+}
