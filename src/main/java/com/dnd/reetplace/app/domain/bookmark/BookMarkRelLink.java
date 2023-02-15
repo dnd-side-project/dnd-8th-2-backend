de4bc76b1a9fd53ae3659b38eb7b2fd85b338fee
Wo-ogie <siwall0105@gmail.com>
2023-02-15 13:40:36 +0900

    [FEAT #3] Bookmark Entity 구현

---
+++ src/main/java/com/dnd/reetplace/app/domain/bookmark/BookMarkRelLink.java
@@ -0,0 +1,19 @@
+package com.dnd.reetplace.app.domain.bookmark;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Embeddable;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Embeddable
+public class BookMarkRelLink {
+
+    private String relLink1;
+    private String relLink2;
+    private String relLink3;
+}
