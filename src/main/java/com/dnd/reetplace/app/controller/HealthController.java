d168fee996b988e9a48027a808a28598df0c2f85
HunSeongPark <phunseong@gmail.com>
2023-02-16 16:16:48 +0900

    [FEAT #5] Health check에 대해 활성화 된 profile 확인 로직 추가

--- src/main/java/com/dnd/reetplace/app/controller/HealthController.java
+++ src/main/java/com/dnd/reetplace/app/controller/HealthController.java
@@ -1,15 +1,22 @@
 package com.dnd.reetplace.app.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+
 @RequestMapping("/api")
+@RequiredArgsConstructor
 @RestController
 public class HealthController {
 
+    private final Environment env;
+
     @GetMapping("/health")
     public String healthCheck() {
-        return "health check v1";
+        return "health check v1 : " + Arrays.stream(env.getActiveProfiles()).findFirst().orElse("");
     }
 }
