f2aca06844c74cbc2cc0f016bf20327ccb490b17
HunSeongPark <phunseong@gmail.com>
2023-02-19 22:43:15 +0900

    [REFACTOR #11] AuthController에서 OAuth2Service import 하도록 변경

--- src/main/java/com/dnd/reetplace/app/controller/AuthController.java
+++ src/main/java/com/dnd/reetplace/app/controller/AuthController.java
@@ -1,6 +1,8 @@
 package com.dnd.reetplace.app.controller;
 
 import com.dnd.reetplace.app.dto.auth.LoginResponse;
+import com.dnd.reetplace.app.service.OAuth2Service;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,7 +15,7 @@ public class AuthController {
     private final OAuth2Service oAuth2Service;
 
     @GetMapping("/login/kakao")
-    public ResponseEntity<LoginResponse> kakaoLogin(@RequestHeader("access-token") String accessToken) {
+    public ResponseEntity<LoginResponse> kakaoLogin(@RequestHeader("access-token") String accessToken) throws JsonProcessingException {
         return ResponseEntity.ok(oAuth2Service.kakaoLogin(accessToken));
     }
 }
