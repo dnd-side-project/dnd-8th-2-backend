5c5e6ab31f30c3bfd092ea1d04c917045a8c0409
HunSeongPark <phunseong@gmail.com>
2023-02-19 16:32:53 +0900

    [FEAT #11] Redis 사용을 위한 RedisRepositoryConfig 추가

---
+++ src/main/java/com/dnd/reetplace/app/config/RedisRepositoryConfig.java
@@ -0,0 +1,22 @@
+package com.dnd.reetplace.app.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableRedisRepositories
+public class RedisRepositoryConfig {
+
+    private final RedisProperties redisProperties;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
+    }
+}
