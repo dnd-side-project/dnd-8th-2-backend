b90947324eb9efa1f085f813bee15054e86adbb6
Wo-ogie <siwall0105@gmail.com>
2023-02-19 22:33:42 +0900

    [FEAT #6]  ResponseEntityExceptionHandler에 정의된 default exception을 처리할 수 있도록 구현
    `ResponseEntityExceptionHandler`를 상속받아 `ResponseEntityExceptionHandler`에 정의된 대부분의 exception들을 처리할 수 있도록 exception handler를 구현하였다.

--- src/main/java/com/dnd/reetplace/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/dnd/reetplace/global/exception/GlobalExceptionHandler.java
@@ -1,17 +1,16 @@
 package com.dnd.reetplace.global.exception;
 
-import com.dnd.reetplace.app.dto.common.ErrorResponse;
+import com.dnd.reetplace.app.dto.exception.ErrorResponse;
+import io.micrometer.core.lang.Nullable;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.web.HttpMediaTypeNotAcceptableException;
-import org.springframework.web.HttpMediaTypeNotSupportedException;
-import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.validation.ConstraintViolationException;
 import java.io.PrintWriter;
@@ -21,8 +20,8 @@ import java.util.Objects;
 import static com.dnd.reetplace.global.log.LogUtils.getLogTraceId;
 
 @Slf4j
-@RestControllerAdvice
-public class GlobalExceptionHandler {
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<ErrorResponse> customExceptionHandle(CustomException e) {
@@ -33,41 +32,34 @@ public class GlobalExceptionHandler {
                 .body(new ErrorResponse(e.getErrorCode(), e.getErrorMessage()));
     }
 
-    @ExceptionHandler({
-            MethodArgumentNotValidException.class,
-            ConstraintViolationException.class,
-    })
-    public ResponseEntity<ErrorResponse> validationExceptionHandle(Exception e) {
-        log.error("[{}] Validation Exception: {}", getLogTraceId(), getExceptionStackTrace(e));
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        log.error("[{}] Validation Exception: {}", getLogTraceId(), getExceptionStackTrace(ex));
 
-        GlobalExceptionType exceptionType = GlobalExceptionType.from(e.getClass());
-
-        String errorMessage = exceptionType.getErrorMessage() + " ";
-        if (Objects.nonNull(exceptionType.getType()) && exceptionType.getType().equals(MethodArgumentNotValidException.class)) {
-            errorMessage += ((MethodArgumentNotValidException) e).getAllErrors().get(0).getDefaultMessage();
-        } else {
-            errorMessage += e.getMessage().split(": ")[1];
-        }
-
+        GlobalExceptionType exceptionType = GlobalExceptionType.METHOD_ARGUMENT_NOT_VALID;
+        String errorMessage = exceptionType.getErrorMessage() + " " + ex.getAllErrors().get(0).getDefaultMessage();
         return ResponseEntity
-                .status(exceptionType.getHttpStatus())
+                .status(HttpStatus.UNPROCESSABLE_ENTITY)
                 .body(new ErrorResponse(exceptionType.getErrorCode(), errorMessage));
     }
 
-    @ExceptionHandler({
-            HttpMessageNotReadableException.class,
-            HttpRequestMethodNotSupportedException.class,
-            HttpMediaTypeNotAcceptableException.class,
-            HttpMediaTypeNotSupportedException.class,
-            MissingServletRequestParameterException.class
-    })
-    public ResponseEntity<ErrorResponse> globalExceptionHandle(Exception e) {
-        log.error("[{}] Global Exception: {}", getLogTraceId(), getExceptionStackTrace(e));
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ErrorResponse> constraintViolationExceptionHandle(ConstraintViolationException ex) {
+        log.error("[{}] Validation Exception: {}", getLogTraceId(), getExceptionStackTrace(ex));
 
-        GlobalExceptionType exceptionType = GlobalExceptionType.from(e.getClass());
-
+        GlobalExceptionType exceptionType = GlobalExceptionType.CONSTRAINT_VIOLATION;
         return ResponseEntity
-                .status(exceptionType.getHttpStatus())
+                .status(HttpStatus.UNPROCESSABLE_ENTITY)
+                .body(new ErrorResponse(exceptionType.getErrorCode(), exceptionType.getErrorMessage()));
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, @Nullable Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        log.error("[{}] Basic Exception: {}", getLogTraceId(), getExceptionStackTrace(ex));
+
+        GlobalExceptionType exceptionType = GlobalExceptionType.from(ex.getClass());
+        return ResponseEntity
+                .status(status)
                 .body(new ErrorResponse(exceptionType.getErrorCode(), exceptionType.getErrorMessage()));
     }
 
