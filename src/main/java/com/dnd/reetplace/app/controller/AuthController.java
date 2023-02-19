93ff87cb213420366acf7ecf6affdc642fcbda6f
HunSeongPark <phunseong@gmail.com>
2023-02-20 00:28:55 +0900

    [FEAT #11] 토큰 재발급을 위한 refresh 엔드포인트 추가

--- src/main/java/com/dnd/reetplace/app/controller/AuthController.java
+++ src/main/java/com/dnd/reetplace/app/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.dnd.reetplace.app.controller;
 
 import com.dnd.reetplace.app.dto.auth.LoginResponse;
+import com.dnd.reetplace.app.dto.auth.TokenResponse;
 import com.dnd.reetplace.app.service.OAuth2Service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
@@ -20,5 +23,10 @@ public class AuthController {
     @GetMapping("/login/kakao")
     public ResponseEntity<LoginResponse> kakaoLogin(@RequestHeader("access-token") String accessToken) throws JsonProcessingException {
         return ResponseEntity.ok(oAuth2Service.kakaoLogin(accessToken));
+    }
+
+    @GetMapping("/refresh")
+    public ResponseEntity<TokenResponse> refresh(HttpServletRequest request) {
+        return ResponseEntity.ok(oAuth2Service.refresh(request));
     }
 }
