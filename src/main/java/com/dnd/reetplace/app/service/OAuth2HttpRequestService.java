a5fd4e77e15bc011baa880b788fa71da5a3eb193
HunSeongPark <phunseong@gmail.com>
2023-02-20 20:21:26 +0900

    [REFACTOR #11] 카카오 로그인 http request 서비스 분리

---
+++ src/main/java/com/dnd/reetplace/app/service/OAuth2HttpRequestService.java
@@ -0,0 +1,52 @@
+package com.dnd.reetplace.app.service;
+
+import com.dnd.reetplace.app.dto.auth.response.KakaoProfileResponse;
+import com.dnd.reetplace.global.exception.auth.KakaoUnauthorizedException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
+@Slf4j
+@Service
+public class OAuth2HttpRequestService {
+
+    public static final String KAKAO_GET_PROFILE_URL = "https://kapi.kakao.com/v2/user/me";
+
+    public KakaoProfileResponse getKakaoProfile(String accessToken) {
+        try {
+            // Header 추가
+            HttpHeaders header = new HttpHeaders();
+            header.add(AUTHORIZATION, "Bearer " + accessToken);
+            header.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+            // request 구성
+            RestTemplate rt = new RestTemplate();
+            rt.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
+            HttpEntity<MultiValueMap<String, String>> httpRequest = new HttpEntity<>(header);
+
+            // API 요청
+            ResponseEntity<String> response = rt.exchange(
+                    KAKAO_GET_PROFILE_URL,
+                    HttpMethod.POST,
+                    httpRequest,
+                    String.class
+            );
+
+            // response 객체에 매핑
+            ObjectMapper mapper = new ObjectMapper();
+            return mapper.readValue(response.getBody(), KakaoProfileResponse.class);
+        } catch (Exception e) {
+            log.error("OAuth2HttpRequestService.getKakaoProfile() ex={}", String.valueOf(e));
+            throw new KakaoUnauthorizedException();
+        }
+    }
+}
--- src/main/java/com/dnd/reetplace/app/service/OAuth2Service.java
+++ src/main/java/com/dnd/reetplace/app/service/OAuth2Service.java
@@ -7,25 +7,14 @@ import com.dnd.reetplace.app.dto.auth.response.LoginResponse;
 import com.dnd.reetplace.app.dto.auth.response.TokenResponse;
 import com.dnd.reetplace.app.repository.MemberRepository;
 import com.dnd.reetplace.app.type.LoginType;
-import com.dnd.reetplace.global.exception.auth.KakaoUnauthorizedException;
 import com.dnd.reetplace.global.security.TokenProvider;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
-
-import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
 @Slf4j
 @Transactional(readOnly = true)
@@ -34,10 +23,10 @@ import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 @Service
 public class OAuth2Service {
 
-    public static final String GET_PROFILE_URL = "https://kapi.kakao.com/v2/user/me";
     private final MemberRepository memberRepository;
     private final RefreshTokenRedisService refreshTokenRedisService;
     private final TokenProvider tokenProvider;
+    private final OAuth2HttpRequestService httpRequestService;
 
     /**
      * 카카오 서버에서 받은 access token을 기반으로 사용자 프로필을 받아온 후, 로그인 (또는 회원가입)을 진행한다.
@@ -47,53 +36,30 @@ public class OAuth2Service {
      */
     @Transactional
     public LoginResponse kakaoLogin(String token) {
-        try {
-            // Header 추가
-            HttpHeaders header = new HttpHeaders();
-            header.add(AUTHORIZATION, "Bearer " + token);
-            header.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
 
-            // request 구성
-            RestTemplate rt = new RestTemplate();
-            rt.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
-            HttpEntity<MultiValueMap<String, String>> httpRequest = new HttpEntity<>(header);
+        KakaoProfileResponse kakaoProfile = httpRequestService.getKakaoProfile(token);
+        String uid = kakaoProfile.getId().toString();
 
-            // API 요청
-            ResponseEntity<String> response = rt.exchange(
-                    GET_PROFILE_URL,
-                    HttpMethod.POST,
-                    httpRequest,
-                    String.class
-            );
+        // 로그인 또는 회원가입 처리
+        Member member = memberRepository.findByUidAndLoginType(uid, LoginType.KAKAO)
+                .orElse(null);
 
-            // response 객체에 매핑
-            ObjectMapper mapper = new ObjectMapper();
-            KakaoProfileResponse kakaoProfile = mapper.readValue(response.getBody(), KakaoProfileResponse.class);
-            String uid = kakaoProfile.getId().toString();
-
-            // 로그인 또는 회원가입 처리
-            Member member = memberRepository.findByUidAndLoginType(uid, LoginType.KAKAO)
-                    .orElse(null);
-
-            if (member == null) {
-                String email = (String) kakaoProfile.getKakao_account().get("email");
-                Map<String, Object> profile = (Map<String, Object>) kakaoProfile.getKakao_account().get("profile");
-                member = Member.builder()
-                        .uid(uid)
-                        .loginType(LoginType.KAKAO)
-                        .email(email)
-                        .nickname((String) profile.get("nickname"))
-                        .build();
-                memberRepository.save(member);
-            }
-            String accessToken = tokenProvider.createAccessToken(member.getUid(), member.getLoginType());
-            String refreshToken = tokenProvider.createRefreshToken(member.getUid(), member.getLoginType());
-            refreshTokenRedisService.saveRefreshToken(member.getUid(), refreshToken);
-            return LoginResponse.of(member, accessToken, refreshToken);
-        } catch (Exception e) {
-            log.error("OAuth2Service.kakaoLogin() ex={}", String.valueOf(e));
-            throw new KakaoUnauthorizedException();
+        if (member == null) {
+            String email = (String) kakaoProfile.getKakao_account().get("email");
+            Map<String, Object> profile = (Map<String, Object>) kakaoProfile.getKakao_account().get("profile");
+            member = Member.builder()
+                    .uid(uid)
+                    .loginType(LoginType.KAKAO)
+                    .email(email)
+                    .nickname((String) profile.get("nickname"))
+                    .build();
+            memberRepository.save(member);
         }
+
+        String accessToken = tokenProvider.createAccessToken(member.getUid(), member.getLoginType());
+        String refreshToken = tokenProvider.createRefreshToken(member.getUid(), member.getLoginType());
+        refreshTokenRedisService.saveRefreshToken(member.getUid(), refreshToken);
+        return LoginResponse.of(member, accessToken, refreshToken);
     }
 
     /**
