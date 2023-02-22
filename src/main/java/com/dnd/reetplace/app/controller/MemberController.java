77b24fa2e273e86d4d70e1ee40bf4d6ac2e1e19c
HunSeongPark <phunseong@gmail.com>
2023-02-22 17:52:03 +0900

    [DOCS #24] 사용자 정보 조회 Swagger 문서 작성

--- src/main/java/com/dnd/reetplace/app/controller/MemberController.java
+++ src/main/java/com/dnd/reetplace/app/controller/MemberController.java
@@ -4,6 +4,10 @@ import com.dnd.reetplace.app.dto.member.MemberDto;
 import com.dnd.reetplace.app.dto.member.response.MemberResponse;
 import com.dnd.reetplace.app.service.MemberService;
 import com.dnd.reetplace.global.security.MemberDetails;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -14,10 +18,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/members")
 @RequiredArgsConstructor
 @RestController
+@Tag(name = "사용자", description = "사용자 관련 API입니다.")
 public class MemberController {
 
     private final MemberService memberService;
 
+    @Operation(summary = "로그인한 사용자 정보 조회", description = "로그인한 사용자의 정보를 조회합니다.")
+    @Parameter(
+            name = "Authorization", description = "사용자의 Access Token", in = ParameterIn.HEADER,
+            required = true, example = "Bearer eyJ0eXAi..."
+    )
     @GetMapping("/my")
     public ResponseEntity<MemberResponse> getLoginMemberInfo(
             @AuthenticationPrincipal MemberDetails memberDetails
