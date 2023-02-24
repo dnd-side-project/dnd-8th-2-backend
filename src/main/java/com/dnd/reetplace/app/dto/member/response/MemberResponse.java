f93da0516e31609825853eca0f8bb99937b4e65c
HunSeongPark <phunseong@gmail.com>
2023-02-24 13:12:50 +0900

    [DOCS #24] 프로필 조회 DTO 관련 Swagger Schema 작성

--- src/main/java/com/dnd/reetplace/app/dto/member/response/MemberResponse.java
+++ src/main/java/com/dnd/reetplace/app/dto/member/response/MemberResponse.java
@@ -2,6 +2,7 @@ package com.dnd.reetplace.app.dto.member.response;
 
 import com.dnd.reetplace.app.dto.member.MemberDto;
 import com.dnd.reetplace.app.type.LoginType;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -10,9 +11,16 @@ import lombok.Getter;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class MemberResponse {
 
+    @Schema(description="사용자 고유 id", example="1")
     private Long memberId;
+
+    @Schema(description="사용자 고유 uid", example="12842032")
     private String uid;
+
+    @Schema(description="로그인한 사용자의 SNS 형식", example="KAKAO")
     private LoginType loginType;
+
+    @Schema(description="사용자 닉네임", example="홍길동")
     private String nickname;
 
     public static MemberResponse from(MemberDto dto) {
