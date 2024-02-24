f0e3f2c0c235f03f348324450b13c4812291486e
HunSeongPark <phunseong@gmail.com>
2024-02-24 21:32:23 +0900

    [FEAT #126] test

--- src/main/java/com/dnd/reetplace/app/controller/TermsController.java
+++ src/main/java/com/dnd/reetplace/app/controller/TermsController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class TermsController {
 
-    @GetMapping("/terms2")
+    @GetMapping("/terms")
     public String terms() {
         return "terms";
     }
